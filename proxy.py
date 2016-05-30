48db7a5d19bafec94d17b7fc95e05e0782641ff0
Con Dmitrik <con@dmitrik.net>
2016-05-30 18:31:36 +0300

    Second one

--- proxy.py
+++ proxy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from collections import defaultdict
 
 
 """ Python meta """
@@ -15,12 +16,25 @@ class A(object):
 class Proxy(object):
 
     def __init__(self, target):
-        self.target = target
+        super(Proxy, self).__setattr__('target', target)
+        super(Proxy, self).__setattr__('calls', defaultdict(lambda: 0))
 
+    def __getattr__(self, name):
+        return getattr(self.target, name)
+
+    def __setattr__(self, name, value):
+        self.calls[name] += 1
+        setattr(self.target, name, value)
+
+    def __str__(self):
+        return str(self.calls)
+    
 def main():
     proxy = Proxy(A())
     proxy.phrase = 'Hello World!'
+    proxy.sdfa = 1
     proxy.test()
+    print str(proxy.phrase)
 
 if __name__ == "__main__":
     main()
