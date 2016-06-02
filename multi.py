6e141a0d6a001f00be1d8098753c80bc717b3234
Con Dmitrik <con@dmitrik.net>
2016-06-02 11:34:36 +0300

    Adding target file

--- multi.py
+++ multi.py
@@ -14,11 +14,20 @@ def main():
     parser = argparse.ArgumentParser(description='Producers/consumers')
     parser.add_argument('--threads', help='Number of threads', default=2, type=int)
     parser.add_argument('--file', help='File to read', default='alice.txt', type=str)
+    parser.add_argument('--file', help='File to read', default='alice.txt', type=str)
     args = parser.parse_args()
     if args.file and args.file is not 'alice.txt':
         source_file = args.file
         if not os.path.exists(source_file):
             print "No file to read. Please check:", source_file
+            exit()
+    else:
+        source_file = args.file
+    with file(source_file, 'r') as source:
+        for line in source:
+            print line
+        
+
 
 if __name__ == "__main__":
     main()
